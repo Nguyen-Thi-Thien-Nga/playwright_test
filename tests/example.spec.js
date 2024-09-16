31c95d3b353cf51397d8da964e00e555745ddaac
Thiên Nga <thiennga@MacBook-Pro-cua-Mac.local>
2024-09-16 14:39:42 +0700

    Push script login Playwright

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,17 +6,8 @@ test('Login', async ({ page }) => {
   await page.locator("//input[@name='uid']").fill("mngr589400");
   await page.locator("//input[@name='password']").fill("arYtege");
   await page.locator("//input[@name='btnLogin']").click();
-  await expect( page.locator("//td[text()='Manger Id : mngr589400']")).toContainText("Manger Id : mngr589400");
+ // await expect( page.locator("//td[text()='Manger Id : mngr589400']")).toContainText("Manger Id : mngr589400");
+  await expect( page.locator("marquee.heading3")).toContainText("Welcome To Manager's Page of Guru99 Bank");
+
 });
-
-/*test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-*/
 
