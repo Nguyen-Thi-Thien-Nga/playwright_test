e43fae4444ba270594544daeca8b79fb424976f1
Thien Nga <thiennga12t1@gmail.com>
2024-09-20 11:20:43 +0700

    add testcase ChangePassword,CustomizedStatement

--- tests/e2e/ChangePassword.spec.js
+++ tests/e2e/ChangePassword.spec.js
@@ -5,7 +5,7 @@ import { ChangePasswordPage } from '../pages/ChangePasswordPage';
 test('Change Password successfully', async ({ page }) => {
     const changePasswordPage = new ChangePasswordPage(page);
     await changePasswordPage.loginStep(process.env.SECOND_USERNAME, process.env.SECOND_PASSWORD);
-    const password = '123456!';
+    const password = 'EpaqUde@1';
     await changePasswordPage.navigateChangePassword();
     await changePasswordPage.changePassword(process.env.SECOND_PASSWORD, password);
     await changePasswordPage.verifyChangePassword();
