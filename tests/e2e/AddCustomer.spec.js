fc095cd0b5ecc32e2f9e8fbb314253ec731253dd
Thien Nga <thiennga12t1@gmail.com>
2024-09-21 22:57:48 +0700

    Test prettypretty

--- tests/e2e/AddCustomer.spec.js
+++ tests/e2e/AddCustomer.spec.js
@@ -1,4 +1,4 @@
-import { test,expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { AddCustomerPage } from "../pages/AddCustomerPage";
 
 const genders = ["Male", "Female"];
