aafba4a610f586079ec7177a03c16832668dc93d
Thien Nga <thiennga12t1@gmail.com>
2024-09-21 23:11:51 +0700

    Add prettierrc.json

--- tests/e2e/AddCustomer.spec.js
+++ tests/e2e/AddCustomer.spec.js
@@ -1,12 +1,12 @@
-import { test } from "@playwright/test";
-import { AddCustomerPage } from "../pages/AddCustomerPage";
+import { test } from '@playwright/test';
+import { AddCustomerPage } from '../pages/AddCustomerPage';
 
-const genders = ["Male", "Female"];
+const genders = ['Male', 'Female'];
 for (const gender of genders) {
   test(`Add customer with gender ${gender}  successfully`, async ({ page }) => {
     const addCustomerPage = new AddCustomerPage(page);
     await addCustomerPage.loginStep(process.env.USERNAME, process.env.PASSWORD);
-    if (gender === "Male") {
+    if (gender === 'Male') {
       await addCustomerPage.addCustomerWithGenderMale();
     } else {
       await addCustomerPage.addCustomerWithGenderFemale();
